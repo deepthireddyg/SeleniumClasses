ad62d384dc861f021f61904b99beabee2f0b997e
Deepthi <deepthireddyg@gmail.com>
2023-03-10 12:41:00 -0500

    Drivers

--- src/BrowserDemo.java
+++ src/BrowserDemo.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32 (1)");
+		WebDriver driver=new ChromeDriver();
 
 	}
 
--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,6 @@
  *
  */
 module SeleniumClasses {
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.chrome_driver;
 }
